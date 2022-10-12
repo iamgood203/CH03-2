0dc57de69d57f9c37a9f17056d9dba96138cdf0c
iamgood203 <115600286+iamgood203@users.noreply.github.com>
2022-10-12 11:59:11 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+
+
+int main(void) {
+	int num = 0;
+	int space;
+	
+	while (num < 5) {
+        space =0;
+        while (space <= num){
+        	printf("*");
+        	space = space + 1;
+		}
+		printf("\n");
+		
+		num = num +1; 
+	
+	int nnum =5;	
+	while (nnum = 5) {
+	    printf("*");
+	    space = space +1;
+	    
+	}
+	return 0;
+}
+
